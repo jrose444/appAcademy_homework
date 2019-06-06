10ec80f5ca72a6a02db5d0f0666ff3c9bacef067
Jeremy <Jrmyrosenberg@gmail.com>
2019-06-05 21:38:59 -0700

    w4d4

--- w3d3_rails/Person/db/migrate/20190529153036_create_people_table2.rb
+++ w3d3_rails/Person/db/migrate/20190529153036_create_people_table2.rb
@@ -1,6 +1,8 @@
 class CreatePeopleTable2 < ActiveRecord::Migration[5.1]
   def change
     create_table :people_table2s do |t|
+      t.string :name, :house_id null: false
+      t.timestamps
     end
   end
 end
